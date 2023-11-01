aa244bc9a408cd8de6595dc29304e886787e350a
wahyuindra11 <wahyuindrapermana99645@gmail.com>
2023-11-01 11:35:55 +0700

    make validator create finishgood

--- resources/views/Finish_Good/form.blade.php
+++ resources/views/Finish_Good/form.blade.php
@@ -1,12 +1,14 @@
-<div class="modal fade" id="modal-form" tabindex="1" role="dialog" aria-hidden="true" data-backdrop="static">
+<div class="modal fade" id="modal-form" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static">
     <div class="modal-dialog">
         <div class="modal-content">
-            <form  id="form-item" method="post" class="form-horizontal" data-toggle="validator" enctype="multipart/form-data" >
-                {{ csrf_field() }} {{ method_field('POST') }}
+            <form id="form-item" method="post" class="form-horizontal" data-toggle="validator" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                {{ method_field('POST') }}
 
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span></button>
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                     <h3 class="modal-title"></h3>
                 </div>
 
@@ -15,47 +17,40 @@
 
                     <div class="box-body">
                         <div class="form-group">
-                            <label >Nama</label>
-                            @foreach($products as $product)
-                                @if ($product->category_id == 3)
-                                    <option value="{{ $product->name }}">{{ $product->nama }}</option>
-                                @endif
-                            @endforeach
-                           </select> --}}
-                            <input type="text" class="form-control" id="nama" name="nama"  autofocus required>
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control" id="nama" name="nama" autofocus required>
                             <span class="help-block with-errors"></span>
                         </div>
 
                         <div class="form-group">
-                            <label >Price</label>
-                            <input type="text" class="form-control" id="harga_beli" name="harga_beli"   required>
+                            <label for="harga_beli">Price</label>
+                            <input type="text" class="form-control" id="harga_beli" name="harga_beli" required>
                             <span class="help-block with-errors"></span>
                         </div>
 
                         <div class="form-group">
-                            <label >Quantity</label>
-                            <input type="text" class="form-control" id="qty" name="qty"   required>
+                            <label for="qty">Quantity</label>
+                            <input type="text" class="form-control" id="qty" name="qty" required>
                             <span class="help-block with-errors"></span>
                         </div>
 
                         <div class="form-group">
-                            <label >Nomer SPB</label>
-                            <input type="text" class="form-control" id="nomer_spb" name="nomer_spb"  autofocus required>
+                            <label for="nomer_spb">Nomer SPB</label>
+                            <input type="text" class="form-control" id="nomer_spb" name="nomer_spb" autofocus required>
                             <span class="help-block with-errors"></span>
                         </div>
 
                         <div class="form-group">
-                            <label >Keterangan</label>
-                            <input type="text" class="form-control" id="keterangan" name="keterangan"  autofocus>
+                            <label for="keterangan">Keterangan</label>
+                            <input type="text" class="form-control" id="keterangan" name="keterangan" autofocus>
                             <span class="help-block with-errors"></span>
                         </div>
 
                         <div class="form-group">
-                            <label >Category</label>
+                            <label for="category_id">Category</label>
                             {!! Form::select('category_id', $category, null, ['class' => 'form-control select', 'placeholder' => '-- Choose Category --', 'id' => 'category_id', 'required']) !!}
                             <span class="help-block with-errors"></span>
                         </div>
-
 
                     </div>
                     <!-- /.box-body -->
