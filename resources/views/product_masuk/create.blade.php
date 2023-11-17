2aba96c09c22c81311bfd48bc7438394ba14333d
Alfa Indica <95673915+Indicartans@users.noreply.github.com>
2023-11-17 13:05:14 +0700

    Update products in

--- resources/views/product_masuk/create.blade.php
+++ resources/views/product_masuk/create.blade.php
@@ -207,7 +207,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Kirim formulir menggunakan AJAX
         $.ajax({
-            url: "/productsOut", // Ganti dengan URL yang sesuai
+            url: "/productsIn", // Ganti dengan URL yang sesuai
             type: "POST",
             data: $(form).serialize(), // Ambil data formulir
             success: function (data) {
